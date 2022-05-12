aa92200eda637589064ba205b63f896490ac2ac2
karankarthic.n <karankarthic.n@zohocorp.com>
2022-05-12 17:01:42 +0530

    Initial Commit

---
+++ EMICalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EMICalculator
+//
+//  Created by Karan Karthic Neelamegan on 12/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
