8eddf6c13207b9bcb6a7391ce31765741fef8e50
karankarthic <44661868+karankarthic@users.noreply.github.com>
2022-05-13 13:12:59 +0530

    changes

--- EMICalculator/ViewController.swift
+++ EMICalculator/ViewController.swift
@@ -15,26 +15,5 @@ class ViewController: UIViewController {
         
     }
 
-    
-    func emiCalculator(la :Double,ir:Double,noy:Double){
-        
-        let  LoanAmount : Double = la; //loan amount
-        let  InterestRate: Double = ir; //interest rate
-        let   NumberOfYears: Double = noy; //number of years
-
-        let   interestRateDecimal: Double = InterestRate / (12 * 100);
-        let    months: Double = NumberOfYears * 12;
-        let   rPower: Double = pow(1+interestRateDecimal,months);
-
-        let  monthlyPayments: Double = LoanAmount * interestRateDecimal * rPower / (rPower - 1);
-        let  totalPayments: Double = monthlyPayments * months;
-        let  yearlyPayments: Double = monthlyPayments * 12;
-        let  totalInterest: Double = totalPayments - LoanAmount;
-        
-        print((totalInterest).rounded())
-        print((monthlyPayments).rounded())
-        print((totalPayments).rounded())
-    }
-
 }
 
