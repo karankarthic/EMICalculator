c948a45a7d168bf8c8fc0c76933bb851111bf500
karankarthic <44661868+karankarthic@users.noreply.github.com>
2022-05-17 15:24:23 +0530

    arc tested

--- EMICalculator/ViewController.swift
+++ EMICalculator/ViewController.swift
@@ -8,11 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var person:Person?
+    var pet:Pet?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        person = Person()
+        pet = Pet()
+        
+        person?.pet = pet
+        pet?.owner = person
+        
+        
+        person = nil
+        
+        print(pet?.owner)
+        
+    }
+    
+}
+
+class Person{
+    
+    var name = "st"
+    var pet:Pet?
+    init() {
+        
     }
 
+    deinit {
+        print("person")
+    }
+    
+    
+}
+
+class Pet{
+
+    var name = "strr"
+    weak var owner:Person?
+    init() {
+        
+    }
+    
+    deinit {
+        print("pet")
+    }
 }
 
